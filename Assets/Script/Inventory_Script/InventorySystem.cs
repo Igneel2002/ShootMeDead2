a3694d1525e9c1947d24ce957af263e40bbcf87d
Igneel2002 <79296259+Igneel2002@users.noreply.github.com>
2021-11-25 12:11:28 +1100

    yes

---
+++ Assets/Script/Inventory_Script/InventorySystem.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySystem : MonoBehaviour
+{
+    public AppleSpawner[] app1;
+    public Shop SHOP;
+    public Menu MENU;
+    [SerializeField]
+    private GameObject Invent;
+    [SerializeField]
+    private GameObject AppleSlot;
+    public GameObject drop;
+    public bool inv;
+    public Text Appletext;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invent.SetActive(false);
+        inv = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("i"))
+        {
+            print("apple" + SHOP.apple);
+            if (inv == true)
+            {
+                Screen.lockCursor = false;
+                Cursor.visible = true;
+                inv = false;
+                Invent.SetActive(true);
+                Time.timeScale = 0;
+                if(SHOP.apple == 0)
+                {
+                    AppleSlot.SetActive(false);
+                }
+                else if (SHOP.apple != 0)
+                {
+                    AppleSlot.SetActive(true);
+                }
+            }
+
+            else if (inv != true)
+            {
+                Screen.lockCursor = true;
+                Cursor.visible = false;
+                Invent.SetActive(false);
+                Time.timeScale = 1;
+                inv = true;
+            }
+        }
+        Appletext.text = "Apples " + (int)SHOP.apple;
+    }
+
+    public void use()
+    {
+        if(SHOP.apple >= 1)
+        {
+            SHOP.apple -= 1;
+            MENU.HEALTH += 10;
+            if(SHOP.apple == 0)
+            {
+                AppleSlot.SetActive(false);
+            }
+            else if(SHOP.apple != 0)
+            {
+                AppleSlot.SetActive(true);
+            }
+        }
+    }
+
+    public void Drop()
+    {
+        if(SHOP.apple >= 1)
+        {
+            SHOP.apple -= 1;
+            AppleSpawner apple = Instantiate(app1[Random.Range(0, app1.Length)], drop.transform.position, Quaternion.identity, transform);
+            transform.position = drop.transform.position;
+            if(SHOP.apple == 0)
+            {
+                AppleSlot.SetActive(false);
+            }
+            else if (SHOP.apple != 0)
+            {
+                AppleSlot.SetActive(true);
+            }
+        }
+    }
+}
